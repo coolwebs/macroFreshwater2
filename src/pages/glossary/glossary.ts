a897e09c1a5f6595e20062cdd6224789550b7ab8
coolwebs <ryan@coolwebs.com.au>
2018-02-07 22:42:34 +1000

    Import glossary json feed into glossary page and setup dynamic list

--- src/pages/glossary/glossary.ts
+++ src/pages/glossary/glossary.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from "@angular/http";
+import "rxjs/add/operator/map";
 
 @Component({
   selector: 'page-glossary',
   templateUrl: 'glossary.html'
 })
 export class GlossaryPage {
+  public glossaryTerms = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(private http: Http, public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+    this.http.get('./assets/appGlossary.json')
+        .map(response => response.json())
+        //.subscribe(data => console.log(data));
+        .subscribe((response) => {
+          this.glossaryTerms = response;
+        });
   }
 
 }
