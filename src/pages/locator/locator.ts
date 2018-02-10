f23d0a2b17a155e07e4ba87b4a83018c240daebb
coolwebs <ryan@coolwebs.com.au>
2018-02-10 10:53:44 +1000

    Add a demo of shufflejs to the locator page

--- src/pages/locator/locator.ts
+++ src/pages/locator/locator.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as $ from "jquery";
+import Shuffle from 'shufflejs';
 
 @Component({
   selector: 'page-locator',
   templateUrl: 'locator.html'
 })
 export class LocatorPage {
-
   constructor(public navCtrl: NavController) {
 
   }
 
+  ionViewDidLoad() {
+    var shuffleInstance = new Shuffle(document.getElementById('grid'), {
+      itemSelector: '.js-item',
+      sizer: '.js-shuffle-sizer'
+    });
+
+    $('#filter li a').click(function (e) {
+      e.preventDefault();
+
+      // set active class
+      $('#filter li a').removeClass('active');
+      $(this).addClass('active');
+
+      // get group name from clicked item
+      var groupName = $(this).attr('data-group');
+      console.log(groupName);
+
+      // reshuffle grid
+      shuffleInstance.filter( groupName );
+    });
+  }
+
 }
+
