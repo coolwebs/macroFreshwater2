ab5ce897344fb7475d388f35557597799568f318
coolwebs <ryan@coolwebs.com.au>
2018-02-11 10:33:57 +1000

    Initially add google maps API to locator view

--- src/pages/locator/locator.ts
+++ src/pages/locator/locator.ts
@@ -2,17 +2,28 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import * as $ from "jquery";
 import Shuffle from 'shufflejs';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+declare var google;
 
 @Component({
   selector: 'page-locator',
   templateUrl: 'locator.html'
 })
 export class LocatorPage {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   constructor(public navCtrl: NavController) {
 
   }
 
   ionViewDidLoad() {
+
+    // Load the google map
+    this.loadMap();
+
     var shuffleInstance = new Shuffle(document.getElementById('grid'), {
       itemSelector: '.js-item',
       sizer: '.js-shuffle-sizer'
@@ -32,6 +43,21 @@ export class LocatorPage {
       // reshuffle grid
       shuffleInstance.filter( groupName );
     });
+  }
+
+  // Function for loading the google map and getting geolocation coords
+  loadMap(){
+
+    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
   }
 
 }
